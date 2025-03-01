de38a7b0f3294a562f0b7fe5ffeb2e003ba2bcd7
Martin <hello@northrook.com>
2025-03-01 07:08:09 +0000

    mend

--- src/Toast.php
+++ src/Toast.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Core\Symfony;
 
 use Core\Interface\ActionInterface;
-use Core\Symfony\{ToastService};
 use JetBrains\PhpStorm\ExpectedValues;
 
 final class Toast implements ActionInterface
@@ -41,6 +40,55 @@ final class Toast implements ActionInterface
         $this->getService()->addMessage( $status, $message, $description, $compact, $timeout, $icon );
     }
 
+    public function info(
+        string  $message,
+        ?string $description = null,
+        bool    $compact = false,
+        ?int    $timeout = null,
+    ) : void {
+        $this->__invoke( $this::INFO, $message, $description, $compact, $timeout );
+    }
+
+    public function notice(
+        string  $message,
+        ?string $description = null,
+        bool    $compact = false,
+        ?int    $timeout = null,
+    ) : void {
+        $this->__invoke( $this::NOTICE, $message, $description, $compact, $timeout );
+    }
+
+    public function success(
+        string  $message,
+        ?string $description = null,
+        bool    $compact = false,
+        ?int    $timeout = null,
+    ) : void {
+        $this->__invoke( $this::SUCCESS, $message, $description, $compact, $timeout );
+    }
+
+    public function warning(
+        string  $message,
+        ?string $description = null,
+        bool    $compact = false,
+        ?int    $timeout = null,
+    ) : void {
+        $this->__invoke( $this::WARNING, $message, $description, $compact, $timeout );
+    }
+
+    public function danger(
+        string  $message,
+        ?string $description = null,
+        bool    $compact = false,
+    ) : void {
+        $this->__invoke( $this::ERROR, $message, $description, $compact );
+    }
+
+    /**
+     * @internal
+     *
+     * @return ToastService
+     */
     public function getService() : ToastService
     {
         return $this->toast;
